71a8abd8d5fe561d8ad091db3f4dff3c4f4951f5
Sajda Kabir <sajdakabir@Sajdas-MacBook-Air.local>
2024-02-01 12:04:30 +0530

    interface

--- intro/interface.ts
+++ intro/interface.ts
@@ -4,11 +4,38 @@
 // }
 
 
+// we can use readonly and ? inside interface too.
 interface User {
     readonly dbId: number
     email: string,
     userId: number,
-    googleId?: string
+    googleId?: string,
+    // startTrail: ()=> string,
+    startTrail(): string,
+    getCoupon( couponname: string, value: number): number
+    // getCoupon: (couponname: string, value: number)=>number
 }
+
+const sajda : User = {
+    dbId:12,
+    email: "sajda@gmail.com",
+    userId:45,
+    startTrail: ()=>{
+        return "trail started"
+    },
+    getCoupon: (name:"saju50", off: 50) =>{
+        return 60
+    }
+
+}
+
+console.log(sajda.email);
+
+sajda.email="saju@gmail.com"
+
+const createUser = (u: User)=>{
+    console.log(`this is the user email${u.email}`);
+}
+
 
 export {}
